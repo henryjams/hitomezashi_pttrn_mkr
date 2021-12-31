77f59485b4a36521ae9924317cc103ccdfa9233c
henryjams <hspradley91@gmail.com>
2021-12-31 16:55:23 -0600

    Create file and description.

---
+++ hitomezashi_pttrn.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+~ Hitomezashi pattern generator ~
+
+Inspired by the Numberphile video featuring Ayliean MacDonald discussing
+Hitomezashi patterns and their generation.
+
+source: https://www.youtube.com/watch?v=JbfhzlMk2eY
+
+"""
+
